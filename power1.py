9d3a7f1384447b4845af20bcc4f18a9d5ffb7485
svalenti52 <svalenti52@yahoo.com>
2022-04-15 07:36:09 -0700

    Add power1.py where we are using hist.

---
+++ power1.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 14 06:33:36 2022
+
+@author: svale
+"""
+
+# power1.py
+
+import random as rd
+
+def Z(x,y):
+    return x**y
+
+nr_events = 20000
+
+z_bins = []
+
+for ix in range(nr_events):
+    x = rd.random()
+    y = rd.random()
+    z = Z(x,y)
+    z_bins.append(z)
+
+sorted_z_bins = sorted(z_bins)
+
+import matplotlib.pyplot as plt
+n, bins, patches = plt.hist(sorted_z_bins, 100)
+
+plt.xlabel('Histogram of Z = X**Y')
+plt.ylabel('Numbers in Each Bin (width 0.01)')
+
+plt.show()
